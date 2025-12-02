16d800b90616048e357c028148051e8a7490f1b9
Kairo Naser <kaironaser100@gmail.com>
2025-12-02 21:07:12 +0300

    Create tic_tac_toe.js

---
+++ problem-solving/tic_tac_toe.js
@@ -0,0 +1,16 @@
+// this to take the input from the users 
+const readline = require('readline')
+const rl = readline.createInterface({
+   input: process.stdin,
+   output:process.stdout
+})
+// intialize array for grid as empty 
+ticGrid=[
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+]
+// function that updates the matrix grid
+function placeMark(x, y, mark){
+  rl.question((input)=>{})
+}
